5b3f1762a70462e329563effdb3ec90e66745386
musharraf10 <skmusharaf00@gmail.com>
2025-02-25 22:31:41 +0530

    addded

--- backend/backend/src/routes/transactionRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createTransaction, getTransactions } from '../controller/transactionscontroller.js';
-
-const transactionRouter = express.Router();
-
-transactionRouter.post('/posttransaction', createTransaction);
-transactionRouter.get('/gettransactions', getTransactions);
-
-export default transactionRouter;
